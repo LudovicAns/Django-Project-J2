c42cda39e0ce2181c0d708b9450eb0a363d56b01
LudovicAns <ludovicanselin@gmail.com>
2025-07-09 14:15:58 +0200

    Add `jobs` app to urlpatterns in the main project configuration file

--- asite/asite/urls.py
+++ asite/asite/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('feedback/', include('feedback.urls')),
+    path('jobs/', include('jobs.urls')),
+
 ]
