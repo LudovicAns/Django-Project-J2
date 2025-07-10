8b6455bfe6a91e55a70f2afeffc164919ba50a43
LudovicAns <ludovicanselin@gmail.com>
2025-07-10 11:34:07 +0200

    Enable token-based authentication
    - Added `TokenAuthentication` to `DEFAULT_AUTHENTICATION_CLASSES` in Django REST framework settings for secure API access.
    - Included `rest_framework.authtoken` in `INSTALLED_APPS` and updated the database accordingly.

--- asite/asite/settings.py
+++ asite/asite/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'jobs.apps.JobsConfig',
     'feedbacks.apps.FeedbacksConfig',
     'rest_framework',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -53,6 +54,9 @@ MIDDLEWARE = [
 ]
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
     ]
